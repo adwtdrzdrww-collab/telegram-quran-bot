639bde616b95c680557b43d0515e2b720cc50b5d
adwtdrzdrww-collab <adwtdrzdrww@gmail.com>
2026-07-22 04:04:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Starting Quran Voice Bot...")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from config import BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "السلام عليكم 🌷\nالبوت اشتغل بنجاح."
+    )
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
